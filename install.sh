44e1cd69722e2debf8325acc62b1bf7f0293b58f
TeleOniOn <44299448+TeleOniOn@users.noreply.github.com>
2020-12-10 23:36:47 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ts
 get() {
 rm -fr Tshake.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/TshAkETEAM/tshakex2/master/Tshake.lua"
+wget "https://raw.githubusercontent.com/TH3HILL/tshakex2/master/Tshake.lua"
 lua start.lua
 }
 installall(){
